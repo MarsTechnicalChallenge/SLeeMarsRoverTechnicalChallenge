9d555ce83045a8f94c6ab54d66afc91cb4cbba26
Sai.lee1991 <Sai.lee1991@outlook.com>
2021-10-02 12:31:29 +0100

    Initial Commit

---
+++ SLeeMarsRoverTechnicalChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SLeeMarsRoverTechnicalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
